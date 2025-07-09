a6fc2c4795348cb27ee94527642169c89bd06eb8
BuddyBuddy <honeyhanu7696@gmail.com>
2025-07-09 14:02:57 +0530

    Upload files into Portfolio Project

---
+++ js/portfolio.js
@@ -0,0 +1,7 @@
+
+  document.getElementById('skills-heading').addEventListener('click', function () {
+    const skills = document.getElementById('skills-cards');
+    skills.classList.toggle('skills-show');
+    skills.classList.toggle('skills-hidden');
+  });
+
